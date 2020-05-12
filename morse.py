e22e69106e7128273ac771311d08fda18f04e248
Pokemaniac00 <aric@the5flors.com>
2020-05-12 13:37:04 -0400

    Initial Commit

---
+++ morse.py
@@ -0,0 +1,19 @@
+#!/bin/env python
+
+def main():
+    print("Welcome to Pokemaniac00's Morse Code Encoder & Decoder.")
+    prompt = input("Would you like to encode a message? (respond 'n' to decode a message) (y/n)")
+    if(prompt == "n"):
+        message = input("Input a message you would like to encode: ")
+        encode(message)
+    else:
+        message = input("Input a message you would like to decode: ")
+        decode(message)
+
+def encode(message):
+    
+
+def decode(message):
+    
+
+main()
